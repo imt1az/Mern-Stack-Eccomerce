1277785108126ac92107c855e30d484fb74f56ca
unknown <imtiazemon625@gmail.com>
2023-05-06 12:59:36 +0600

    Paginate

--- server/services/products.service.js
+++ server/services/products.service.js
@@ -135,7 +135,7 @@ const paginateProducts = async (req) => {
     let aggQuery = Product.aggregate(aggQueryArray);
     const options = {
       page: req.body.page,
-      limit: 3,
+      limit: 5,
       sort: { date: "desc" },
     };
     const products = await Product.aggregatePaginate(aggQuery, options);
